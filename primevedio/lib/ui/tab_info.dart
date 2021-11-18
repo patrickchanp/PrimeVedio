dbf2d4bd238a881f06253b10982f1d857fd6e572
pengfei <pengfei.qian@thoughtworks.com>
2021-11-18 20:23:32 +0800

    feat:optimize home page layout

--- primevedio/lib/ui/tab_info.dart
+++ primevedio/lib/ui/tab_info.dart
@@ -15,23 +15,15 @@ class _TabInfoState extends State<TabInfo> {
   @override
   Widget build(BuildContext context) {
     return ListView(
+      padding: EdgeInsets.only(
+          left: UIData.spaceSizeWith24,
+          right: UIData.spaceSizeWith24,
+          top: UIData.spaceSizeWith24),
       children: [
+        SizedBox(height: UIData.spaceSizeHeight17256, child: const ViewPage()),
         Padding(
-          padding: EdgeInsets.only(
-              left: UIData.spaceSizeWith24,
-              right: UIData.spaceSizeWith6622,
-              top: UIData.spaceSizeHeight30),
-          child: const ViewPage(),
-        ),
-        Padding(
-          padding: EdgeInsets.only(
-            left: UIData.spaceSizeWith24,
-            top: UIData.spaceSizeHeight2244,
-            bottom: UIData.spaceSizeHeight1771,
-          ),
-          child: Row(children: [
-            CommonText.normalText('最新发布', textAlign: TextAlign.left),
-          ]),
+          padding: EdgeInsets.symmetric(vertical: UIData.spaceSizeWith16),
+          child: CommonText.normalText('最新发布'),
         ),
         _gridView(),
       ],
@@ -49,7 +41,7 @@ class ViewPage extends StatefulWidget {
 class _ViewPageState extends State<ViewPage> {
   var imgList = [
     'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=2877516247,37083492&fm=26&gp=0.jpg',
-    'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1582796218195&di=04ce93c4ac826e19067e71f916cec5d8&imgtype=0&src=http%3A%2F%2Fhbimg.b0.upaiyun.com%2F344fda8b47808261c946c81645bff489c008326f15140-koiNr3_fw658'
+    'https://pica.zhimg.com/v2-3b4fc7e3a1195a081d0259246c38debc_720w.jpg?source=172ae18b'
   ];
 
   @override
@@ -64,26 +56,19 @@ class _ViewPageState extends State<ViewPage> {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-        height: UIData.spaceSizeHeight17256,
-        width: UIData.spaceSizeWith30678,
-        child: PageView.builder(
-          itemBuilder: (context, index) => _buildPageItem(index),
-          // controller: _pageController,
-        ));
+    return PageView.builder(
+      // pageSnapping: false,
+      itemBuilder: (context, index) => _buildPageItem(index),
+      // controller: _pageController,
+    );
   }
 
   _buildPageItem(int index) {
-    Matrix4 matrix4 = Matrix4.identity();
-
-    return Transform(
-      transform: matrix4,
-      child: Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(ScreenUtil().radius(11.65)),
-          image: DecorationImage(
-              image: NetworkImage(imgList[index % 2]), fit: BoxFit.cover),
-        ),
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(ScreenUtil().radius(11.65)),
+        image: DecorationImage(
+            image: NetworkImage(imgList[index % 2]), fit: BoxFit.fill),
       ),
     );
   }
@@ -93,7 +78,6 @@ Widget _gridView() {
   return GridView.count(
     shrinkWrap: true,
     primary: false,
-    padding: EdgeInsets.symmetric(horizontal: UIData.spaceSizeWith24),
     crossAxisCount: 2,
     childAspectRatio: 0.7,
     mainAxisSpacing: 20,
@@ -113,14 +97,13 @@ Widget _buildReleaseItem(color) {
     mainAxisAlignment: MainAxisAlignment.start,
     children: [
       Container(
-        // color: color,
         decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(ScreenUtil().radius(11.65)),
             color: color),
         height: UIData.spaceSizeHeight202,
-        width: UIData.spaceSizeWith159,
       ),
-      Padding(
+      Container(
+        alignment: Alignment.centerLeft,
         padding: const EdgeInsets.only(top: 12.0),
         child: CommonText.normalText("text"),
       )
